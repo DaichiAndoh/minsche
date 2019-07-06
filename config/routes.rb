5e1b6abcb40cf9a5a3e99883d510ee72d77c53c9
daichi.andoh <daichi.andoh@gmail.com>
2019-07-06 07:34:34 +0000

    member login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'members#new'
   resources :members, only: [:new, :create]
 end
