c26803b99e09b6ea333894aba011ad09e29ead82
daichi.andoh <daichi.andoh@gmail.com>
2019-07-06 05:54:20 +0000

    member signup

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,23 @@
+class MembersController < ApplicationController
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+    
+    if @member.save
+      flash[:success] = '登録しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = '登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get 'signup', to: 'members#new'
+  resources :members, only: [:new, :create]
 end
